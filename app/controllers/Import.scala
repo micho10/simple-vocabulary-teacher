3a73dbc8615731adc7f5febc1b8a0b5de60868f1
micho <gatin13@yahoo.com>
2016-11-04 19:34:28 +0900

    Added some basic request handling. WIP

---
+++ app/controllers/Import.scala
@@ -0,0 +1,16 @@
+package controllers
+
+import play.api.mvc.Controller
+
+/**
+  * Created by carlos on 04/11/16.
+  */
+class Import extends Controller {
+
+  def importWord(
+                sourceLanguage: String,
+                targetLanguage: String,
+                word: String,
+                translation: String
+                ) = TODO
+}
