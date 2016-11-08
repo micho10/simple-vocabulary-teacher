c29c4c1f8a7298fd0c60cdc6ff6bf5d7f0b6cd51
micho <gatin13@yahoo.com>
2016-11-08 16:55:41 +0900

    Implemented the quiz controller Actions

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -1,13 +1,16 @@
 package controllers
 
+import javax.inject.Inject
+
+import models.Vocabulary
 import play.api.i18n.Lang
-import play.api.mvc.Controller
-import play.mvc.Http.Context.Implicit
+import play.api.mvc.{ Action, Controller }
+import services.VocabularyService
 
 /**
  * Created by carlos on 07/11/16.
  */
-class Quiz @Implicit extends Controller {
+class Quiz @Inject() (vocabulary: VocabularyService) extends Controller {
 
   /**
    *
@@ -16,7 +19,13 @@ class Quiz @Implicit extends Controller {
    * @return 200 OK result that wraps a random word, if there is one;
    *         404 Not Found result otherwise
    */
-  def quiz(sourceLanguage: Lang, targetLanguage: Lang) = TODO
+  def quiz(sourceLanguage: Lang, targetLanguage: Lang) = Action { request =>
+    val foundWord = vocabulary.findRandomVocabulary(sourceLanguage, targetLanguage)
+    foundWord match {
+      case Some(_) => Ok
+      case None => NotFound
+    }
+  }
 
   /**
    * An Action that verifies a word.
@@ -28,7 +37,11 @@ class Quiz @Implicit extends Controller {
    * @return 200 Ok result if the translation is correct;
    *         406 Not Acceptable otherwise
    */
-  def check(sourceLanguage: Lang, word: String, targetLanguage: Lang, translation: String) = TODO
+  def check(sourceLanguage: Lang, word: String, targetLanguage: Lang, translation: String) = Action { request =>
+    val correct = vocabulary.verify(sourceLanguage, word, targetLanguage, translation)
+    if (correct) Ok
+    else NotAcceptable
+  }
 
 }
 
