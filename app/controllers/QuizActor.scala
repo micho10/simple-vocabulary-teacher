526fc880d1b16bf77847c2ec9127cc1b4cf7c4fd
micho <gatin13@yahoo.com>
2016-11-16 16:47:55 +0900

    Added a utility method for cProps of a QuizActor

--- app/controllers/QuizActor.scala
+++ app/controllers/QuizActor.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import akka.actor.{Actor, ActorRef}
+import akka.actor.{Actor, ActorRef, Props}
 import play.api.i18n.Lang
 import services.VocabularyService
 
@@ -8,6 +8,11 @@ import services.VocabularyService
   * Created by carlos on 15/11/16.
   *
   * Creates the actor based on the desired languages as well as the reference to the outgoing channel, out
+  *
+  * When a new WebSocket connection is established, Play will automatically create a new instance of this actor
+  * and provide it with an actor reference that represents the outgoing channel.
+  *
+  * Incoming messages are sent by the client, and the actor can react to the in its <i>receive</i> method.
   */
 class QuizActor(out: ActorRef,
                 sourceLang: Lang,
@@ -42,4 +47,19 @@ extends Actor {
       out ! s"I don't know any word for ${sourceLang.code} and ${targetLang.code}"
     }
   }
+
+}
+
+object QuizActor {
+
+  /**
+    * Utility method that returns the <code>Props</code> of an actor, a means of explaining how the actor can be built.
+    * This is the most important part when setting up a WebSocket endpoint.
+    */
+  def props(out: ActorRef,
+            sourceLang: Lang,
+            targetLang: Lang,
+            vocabulary: VocabularyService): Props =
+    Props(classOf[QuizActor], out, sourceLang, targetLang, vocabulary)
+
 }
