a4c23d61dd7486699658c47a69c9a5a500dbddd7
micho <gatin13@yahoo.com>
2016-11-16 17:35:23 +0900

    Added a WebSocket handler method and the GET route to establish a WebSocket connection

--- app/controllers/Quiz.scala
+++ app/controllers/Quiz.scala
@@ -3,8 +3,10 @@ package controllers
 import javax.inject.Inject
 
 import play.api.i18n.Lang
-import play.api.mvc.{ Action, Controller }
+import play.api.mvc.{ Action, Controller, WebSocket }
 import services.VocabularyService
+
+import play.api.Play.current
 
 /**
  * Created by carlos on 07/11/16.
@@ -41,5 +43,18 @@ class Quiz @Inject() (vocabulary: VocabularyService) extends Controller {
     if (correct) Ok
     else NotAcceptable
   }
+
+  // Languages are provided as parameters to the handler method
+  def quizEndPoint(sourceLang: Lang, targetLang: Lang) =
+    // Incoming and outgoing messages are both Strings. To correctly set up the WebSocket connection, Play needs to know
+    // what the encoding of the messages is going to be.
+    WebSocket.acceptWithActor[String, String] {
+      // RequestHeaders of the incoming request can be used to check whether the connection should be established
+      request =>
+        // The actor reference out represents the outgoing channel to the client
+        out =>
+          // The call to the helper method that returns the Props of a QuizActorto be created
+          QuizActor.props(out, sourceLang, targetLang, vocabulary)
+    }
 
 }
--- app/controllers/QuizActor.scala
+++ app/controllers/QuizActor.scala
@@ -1,24 +1,24 @@
 package controllers
 
-import akka.actor.{Actor, ActorRef, Props}
+import akka.actor.{ Actor, ActorRef, Props }
 import play.api.i18n.Lang
 import services.VocabularyService
 
 /**
-  * Created by carlos on 15/11/16.
-  *
-  * Creates the actor based on the desired languages as well as the reference to the outgoing channel, out
-  *
-  * When a new WebSocket connection is established, Play will automatically create a new instance of this actor
-  * and provide it with an actor reference that represents the outgoing channel.
-  *
-  * Incoming messages are sent by the client, and the actor can react to the in its <i>receive</i> method.
-  */
+ * Created by carlos on 15/11/16.
+ *
+ * Creates the actor based on the desired languages as well as the reference to the outgoing channel, out
+ *
+ * When a new WebSocket connection is established, Play will automatically create a new instance of this actor
+ * and provide it with an actor reference that represents the outgoing channel.
+ *
+ * Incoming messages are sent by the client, and the actor can react to the in its <i>receive</i> method.
+ */
 class QuizActor(out: ActorRef,
-                sourceLang: Lang,
-                targetLang: Lang,
-                vocabulary: VocabularyService)
-extends Actor {
+  sourceLang: Lang,
+  targetLang: Lang,
+  vocabulary: VocabularyService)
+    extends Actor {
 
   // Keeps track of which word you're currently asking for a translation of
   private var word = ""
@@ -26,14 +26,13 @@ extends Actor {
   // When starting up, sends a new word to translate
   override def preStart(): Unit = sendWord()
 
-  def  receive = {
-    case translation: String
-      if vocabulary.verify(
-        sourceLang, word, targetLang, translation
-      ) =>
-        out ! "Correct!"
-        // If a correct translation was provided, asks for a new word
-        sendWord()
+  def receive = {
+    case translation: String if vocabulary.verify(
+      sourceLang, word, targetLang, translation
+    ) =>
+      out ! "Correct!"
+      // If a correct translation was provided, asks for a new word
+      sendWord()
     case _ =>
       out ! "Incorrect, try again!"
   }
@@ -41,11 +40,11 @@ extends Actor {
   def sendWord() = {
     vocabulary
       .findRandomVocabulary(sourceLang, targetLang).map { v =>
-      out ! s"Please translate'${v.word}'"
-      word = v.word
-    } getOrElse {
-      out ! s"I don't know any word for ${sourceLang.code} and ${targetLang.code}"
-    }
+        out ! s"Please translate'${v.word}'"
+        word = v.word
+      } getOrElse {
+        out ! s"I don't know any word for ${sourceLang.code} and ${targetLang.code}"
+      }
   }
 
 }
@@ -53,13 +52,13 @@ extends Actor {
 object QuizActor {
 
   /**
-    * Utility method that returns the <code>Props</code> of an actor, a means of explaining how the actor can be built.
-    * This is the most important part when setting up a WebSocket endpoint.
-    */
+   * Utility method that returns the <code>Props</code> of an actor, a means of explaining how the actor can be built.
+   * This is the most important part when setting up a WebSocket endpoint.
+   */
   def props(out: ActorRef,
-            sourceLang: Lang,
-            targetLang: Lang,
-            vocabulary: VocabularyService): Props =
+    sourceLang: Lang,
+    targetLang: Lang,
+    vocabulary: VocabularyService): Props =
     Props(classOf[QuizActor], out, sourceLang, targetLang, vocabulary)
 
 }
